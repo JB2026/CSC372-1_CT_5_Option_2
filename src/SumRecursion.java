51818ee88cb1b2d7c3b15bae79f731c1bd44540b
Joshua Borck <joshua.borck@csuglobal.edu>
2025-01-05 11:30:29 -0500

    Adding new revisions

--- src/SumRecursion.java
+++ src/SumRecursion.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class SumRecursion {
 	
 	// A method that gets a number per number of iterations and returns its sum.
-	public static int findSum(int numberOfIterations, int currentSum, Scanner scnr) {
+	public static double findSum(int numberOfIterations, double currentSum, Scanner scnr) {
 		
 		// Base case and return the sum if the number of iterations is zero
 		if (numberOfIterations <= 0) {
@@ -14,32 +14,32 @@ public class SumRecursion {
 		} else {
 			// Decrease the number of iterations, get the number from the user and call to get the sum
 			numberOfIterations--;
-			int nextNumber = getNumber(scnr);
+			double nextNumber = getNumber(scnr);
 			return findSum(numberOfIterations, currentSum + nextNumber, scnr);
 		}
 	}
 	
 	// A method to get a number from the user or allows them to quit. 
-	public static int getNumber(Scanner scnr) {
+	public static double getNumber(Scanner scnr) {
 		boolean hasValidInput = false;
-		int number = 0;
+		double number = 0.0;
 		
 		while (!hasValidInput) {
 			try {
-				System.out.println("Please enter a whole number or enter \"q\" to quit:");
-				String input = scnr.next();
+				System.out.println("Please enter a number or enter \"q\" to quit:");
+				String input = scnr.next().strip();
 				
 				// If the input is q, exit the program
-				if (input.strip().equals("q")) {
+				if (input.equals("q")) {
 					System.out.println("\nExiting..");
 					System.exit(0);
-				}
+				}				
 				
 				// Get input as a number
-				number = Integer.parseInt(input);
+				number = Double.parseDouble(input);
 				hasValidInput = true;
 				System.out.println();
-			} catch (NoSuchElementException | IllegalStateException | NumberFormatException e) {
+			} catch (NoSuchElementException | IllegalStateException | NumberFormatException | NullPointerException e) {
 				// An incorrect value has been entered and inform the user
 				System.out.println("\nIncorrect value entered. Please try again.\n");
 				scnr.nextLine();
@@ -55,7 +55,7 @@ public class SumRecursion {
 		Scanner scnr = new Scanner(System.in);
 		
 		// Output the sum of 5 numbers the user enters
-		System.out.printf("Sum: %d\n", findSum(5, 0, scnr));
+		System.out.printf("Sum: %.2f\n", findSum(5, 0, scnr));
 		
 		// Close the scanner
 		scnr.close();
